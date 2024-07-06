ae06616ec338f6d9a76643d5d4be0445b0776ded
Oren Kosto <oren.kosto@riskified.com>
2024-07-06 17:34:52 +0300

    checkboxes for the reports view

--- src/MainAppContext.tsx
+++ src/MainAppContext.tsx
@@ -1,19 +1,19 @@
-import React, { createContext, useEffect, useState } from 'react';
-import { calculateSimilarity, DiffReportType, generateDiff } from './generator';
-import { extractTextFromFiles } from './extractor';
-import { Change } from 'diff';
+import React, { createContext, useEffect, useState } from 'react'
+import { calculateSimilarity, DiffReportType, generateDiff } from './generator'
+import { extractTextFromFiles } from './extractor'
+import { Change } from 'diff'
 
 interface IMainAppContext {
-  file1?: File;
-  file2?: File;
-  setFile1: (file: File) => void;
-  setFile2: (file: File) => void;
-  areFilesSelected: boolean;
-  reportType: DiffReportType;
-  setReportType: (type: DiffReportType) => void;
-  texts?: string[];
-  diffReport?: Change[];
-  similarity?: number;
+  file1?: File
+  file2?: File
+  setFile1: (file: File) => void
+  setFile2: (file: File) => void
+  areFilesSelected: boolean
+  reportType: DiffReportType
+  setReportType: (type: DiffReportType) => void
+  texts?: string[]
+  diffReport?: Change[]
+  similarity?: number
 }
 
 const MainAppContext = createContext<IMainAppContext>({
@@ -32,30 +32,30 @@ export const MainAppContextProvider = (props: { children: React.ReactNode }) => 
   const [diffReport, setDiffReport] = useState<Change[] | undefined>([])
   const [similarity, setSimilarity] = useState<number | undefined>()
 
-  const areFilesSelected = !!file1 && !!file2;
+  const areFilesSelected = !!file1 && !!file2
 
   useEffect(() => {
     if (file1) {
-      console.log('file1 selected', file1.name);
+      console.log('file1 selected', file1.name)
     }
-  }, [file1]);
+  }, [file1])
 
   useEffect(() => {
     if (file2) {
-      console.log('file2 selected', file2.name);
+      console.log('file2 selected', file2.name)
     }
-  }, [file2]);
+  }, [file2])
 
   const generateDiffReport = async (type: DiffReportType) => {
     if (areFilesSelected) {
       const texts = await extractTextFromFiles([file1, file2])
-      setTexts(texts);
+      setTexts(texts)
 
-      const diff = await generateDiff(texts[0], texts[1], type);
-      setDiffReport(diff);
+      const diff = await generateDiff(texts[0], texts[1], type)
+      setDiffReport(diff)
 
-      const similarity = await calculateSimilarity(texts[0], texts[1]);
-      setSimilarity(similarity);
+      const similarity = await calculateSimilarity(texts[0], texts[1])
+      setSimilarity(similarity)
     }
   }
 
@@ -63,8 +63,8 @@ export const MainAppContextProvider = (props: { children: React.ReactNode }) => 
     generateDiffReport(reportType)
       .then(() => {
         console.log('Diff report generated')
-      });
-  }, [file1, file2, reportType]);
+      })
+  }, [file1, file2, reportType])
 
   const value: IMainAppContext = {
     file1,
@@ -87,11 +87,11 @@ export const MainAppContextProvider = (props: { children: React.ReactNode }) => 
 }
 
 const useMainAppContext = () => {
-  const context = React.useContext(MainAppContext);
+  const context = React.useContext(MainAppContext)
   if (!context) {
-    throw new Error('useMainAppContext must be used within a MainAppContextProvider');
+    throw new Error('useMainAppContext must be used within a MainAppContextProvider')
   }
-  return context;
+  return context
 }
 
-export default useMainAppContext;
+export default useMainAppContext
--- src/ReportContainer.tsx
+++ src/ReportContainer.tsx
@@ -1,8 +1,9 @@
-import useMainAppContext from './MainAppContext';
-import styled from 'styled-components';
-import { JSONTree } from 'react-json-tree';
-import Row from './Row';
-import DiffViewer from './DiffViewer';
+import useMainAppContext from './MainAppContext'
+import styled from 'styled-components'
+import { JSONTree } from 'react-json-tree'
+import Row from './Row'
+import DiffViewer from './DiffViewer'
+import { useState } from 'react'
 
 const Wrapper = styled.div`
   width: 80%;
@@ -20,34 +21,49 @@ const TreeViewContainer = styled.div`
 const DiffViewContainer = styled.div`
   text-align: left;
   flex: 3;
-  overflow: scroll;
+  overflow: auto;
   background-color: #f5f5f5;
   margin-top: 0.5rem;
 `
 
 const ReportContainer = () => {
-  const { diffReport, similarity = 0, texts } = useMainAppContext();
-  const text1 = texts ? texts[0] : '';
-  const text2 = texts ? texts[1] : '';
+  const { diffReport, similarity = 0, texts } = useMainAppContext()
+  const text1 = texts ? texts[0] : ''
+  const text2 = texts ? texts[1] : ''
+
+  const [showDiffView, setShowDiffView] = useState<boolean>(true)
+  const [showRawDiff, setShowRawDiff] = useState<boolean>(false)
+
   return (
     <Wrapper>
       <Row>
-        Similarity: {similarity.toFixed(2)}
+        Content of the files:
       </Row>
       <Row>
         <textarea rows={20} contentEditable={false} value={text1} onChange={() => {}}/>
         <textarea rows={20} contentEditable={false} value={text2} onChange={() => {}}/>
       </Row>
       <Row>
-        <TreeViewContainer>
-          <JSONTree data={diffReport} hideRoot />
-        </TreeViewContainer>
-        <DiffViewContainer>
-          <DiffViewer />
-        </DiffViewContainer>
+        Similarity: {similarity.toFixed(2)}
+      </Row>
+      <Row>
+        <input type="checkbox" checked={showDiffView} onChange={() => setShowDiffView(!showDiffView)} /> Show diff view
+        <input type="checkbox" checked={showRawDiff} onChange={() => setShowRawDiff(!showRawDiff)} /> Show raw report
+      </Row>
+      <Row>
+        {showDiffView ? (
+          <DiffViewContainer>
+            <DiffViewer/>
+          </DiffViewContainer>
+        ) : undefined}
+        {showRawDiff ? (
+          <TreeViewContainer>
+            <JSONTree data={diffReport} hideRoot/>
+          </TreeViewContainer>
+        ) : undefined}
       </Row>
     </Wrapper>
-  );
+  )
 }
 
-export default ReportContainer;
+export default ReportContainer
